36e1ac23ffaaed2141a7f500262a7b51b6cfeb24
1U23CS126VENNILA <142867008+1U23CS126VENNILA@users.noreply.github.com>
2025-02-15 09:26:04 +0000

    submit2

--- factorialRecursion.java
+++ factorialRecursion.java
@@ -14,9 +14,15 @@ System.out.println("FACTORIAL OF A GIVEN NUMBER IS: " +factorial(n));
 }
 static int factorial(int n)
 {
+if (n==0 || n==1){
+  return 1;
 
-
-//Start your Code Here
-
+} else{
+  return n*factorial(n-1);
 }
 }
+}
+
+
+
+
